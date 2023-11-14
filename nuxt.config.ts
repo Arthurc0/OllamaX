ddebe8be3154ef9ded6741f9653d3b426de04ec0
Arthur <chaff.arthur@gmail.com>
2023-11-14 15:09:41 +0100

    Setup ESLint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config';
+
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    ssr: true
+    ssr: true,
+    typescript: {
+        strict: true,
+        typeCheck: true
+    }
 });
