32672accc0be8e0c2ab5a66f6f271b9a0f02048e
Arthur <chaff.arthur@gmail.com>
2023-12-24 22:28:53 +0100

    Model page (wip)

---
+++ components/model/Card.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="relative group overflow-hidden cursor-pointer border-2 rounded-lg shadow-lg hover:shadow-md transition duration-200 text-gray/70 hover:text-gray/60 flex flex-col" :class="[model?.selected ? 'border-gray/10' : 'border-light-gray']">
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { ModelInterface } from '@/interfaces/model/ModelInterface';
+
+defineProps<{
+    model: ModelInterface;
+}>();
+</script>
--- pages/models/index.vue
+++ pages/models/index.vue
@@ -1,8 +1,19 @@
 <template>
-    <div class="flex flex-col gap-12 p-12">
-        <h1 class="text-gray text-3xl font-semibold">Modèles</h1>
+    <div class="flex flex-1 flex-col gap-12 p-12">
+        <h1 class="text-gray text-3xl font-semibold select-none">Modèles</h1>
         <div class="flex flex-col gap-4">
-            <p>...</p>
+            <AppButton :type="ButtonTypeEnum.ADD_MODEL">
+                <AppIcon :name="IconEnum.ADD" custom-size="25" />
+                <span>Ajouter</span>
+            </AppButton>
+            <h2 class="text-xl font-medium text-gray/80">Sélectionné</h2>
+            <h2 class="text-xl font-medium text-gray/80">Installés</h2>
+            <h2 class="text-xl font-medium text-gray/80">Disponibles</h2>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+import { IconEnum } from '@/enums/IconEnum';
+import { ButtonTypeEnum } from '@/enums/button/ButtonTypeEnum';
+</script>
